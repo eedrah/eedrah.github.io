26f4e6c6fe59ecaef241d3f2532584fea74bc012
Eedrah <eedrah@users.noreply.github.com>
2015-03-22 21:51:17 +1300

    create sliding motion on hover

--- javascripts/sliders.js
+++ javascripts/sliders.js
@@ -1,14 +1,41 @@
-$(bindSliders);
+$(run);
+
+function run(){
+    createJqueryAnimations();
+    bindSliders();
+}
 
 function bindSliders(){
-    $('h3').hide();
+    $('h3').each(initiallyHideText);
     $('li').hover(showText, hideText);
 };
 
+function initiallyHideText(){
+    var $this = $(this);
+    var marginLeft = -$this.outerWidth();
+    $this.css('marginLeft', marginLeft);
+};
+
 function showText(evnt){
-    $(evnt.currentTarget).children().first().children().first().show();
+    console.log('show');
+    $(evnt.currentTarget).children().first().children().first().blindLeftIn(200);
 };
 
 function hideText(evnt){
-    $(evnt.currentTarget).children().first().children().first().hide();
+    console.log('hide');
+    $(evnt.currentTarget).children().first().children().first().blindLeftOut(200);
+};
+
+function createJqueryAnimations(){
+    jQuery.fn.blindLeftOut = function (duration, easing, complete) {
+        return this.animate({
+            marginLeft: -this.outerWidth()
+        }, jQuery.speed(duration, easing, complete));
+    };
+
+    jQuery.fn.blindLeftIn = function (duration, easing, complete) {
+        return this.animate({
+            marginLeft: 0
+        }, jQuery.speed(duration, easing, complete));
+    };
 };
