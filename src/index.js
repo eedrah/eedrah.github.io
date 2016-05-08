1a913efb80c72e5517eaa868020f9a4112091e4f
Eedrah <eedrah@users.noreply.github.com>
2016-05-08 12:46:32 +1200

    accept components and routes from modules

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { browserHistory, Router } from 'react-router'
-var scriptjs = require('scriptjs')
+const scriptjs = require('scriptjs')
 
 const reactApp = document.createElement('div')
 document.body.appendChild(reactApp)
 
 const isProd = process.env.NODE_ENV === 'production'
 
-var getModule = function (module, callback) {
+const getModule = function (module, callback) {
   const address = isProd
     ? `http://eedrah.com/${module}/main.js`
     : 'http://localhost:8080/main.js'
   scriptjs(address, () => {
-    console.log(window[module + 'Var'])
-    callback(null, window[module + 'Var'])
+    callback(null, window[module])
   })
+}
+
+const getComponent = function (module, callback) {
+  getModule(module, (err, moduleExport) => callback(null, moduleExport.component))
+}
+const getChildRoutes = function (module, callback) {
+  getModule(module, (err, moduleExport) => callback(null, moduleExport.childRoutes))
 }
 
 const routes = {
@@ -24,9 +30,8 @@ const routes = {
   childRoutes: [
     {
       path: 'hello',
-      getComponent: (location, callback) => {
-        getModule('HelloRepo', callback)
-      }
+      getComponent: (location, callback) => getComponent('HelloRepo', callback),
+      getChildRoutes: (location, callback) => getChildRoutes('HelloRepo', callback)
     }
   ]
 }
